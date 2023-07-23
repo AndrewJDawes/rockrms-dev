4073392d4b4a75436a4a5677012442f98f7f7098
Andrew Dawes <andrewjamesdawes@gmail.com>
2023-07-23 19:34:43 -0400

    Add dbcreator role to new user

--- provisioners/sqlserver-configure.ps1
+++ provisioners/sqlserver-configure.ps1
@@ -1,10 +1,10 @@
 # https://octopus.com/blog/sql-server-powershell
 # Fill in the values defined here:
-
 $server = "WIN-ROCKSQLSERV"
 $new_user_username = "RockUser"
 $new_user_password = "RockUser"
 $connectionString = "Server=$server;Integrated Security=true;Initial Catalog=master"
+$path = "SQLSERVER:\SQL\$server"
 
 # We will now import the SQL Server Module.
 Import-Module "SqlServer"
@@ -12,10 +12,6 @@ Import-Module "SqlServer"
 # Set up initial connection
 try
 {
-    # This is a simple user/pass connection string.
-    # Feel free to substitute "Integrated Security=True" for system logins.
-    # $connectionString = "Data Source=YourInstance;Database=YourDB;User ID=YourUser;Password=YourPassword"
-
     #Create a SQL connection object
     $conn = New-Object System.Data.SqlClient.SqlConnection $connectionString
 
@@ -25,19 +21,20 @@ try
     {
         # We have a successful connection here
         # Notify of successful connection
-        Write-Host "Test connection successful!"
+        # Write-Host "Test connection successful!"
         $conn.Close()
+    } else {
+        # We could not connect here
+        # Notify there was an error connecting to the database
+        Write-Host "Unable to connect to SQL Server!"
     }
-    # We could not connect here
-    # Notify connection was not in the "open" state
 }
 catch
 {
     # We could not connect here
     # Notify there was an error connecting to the database
-    echo "Unable to connect to SQL Server!"
+    Write-Host "Unable to connect to SQL Server!"
 }
-
 
 # To run in a non-interactive mode, such as through an Octopus deployment, you will most likely need to pass the new login credentials as a PSCredential object.
 $pass = ConvertTo-SecureString $new_user_password -AsPlainText -Force
@@ -47,3 +44,12 @@ $loginCred = New-Object System.Management.Automation.PSCredential($new_user_user
 
 # Create login using the Add-SqlLogin cmdlet
 Add-SqlLogin -ServerInstance $server -LoginPSCredential $loginCred -LoginType SqlLogin -GrantConnectSql -Enable
+
+# Grant dbcreator role to new user
+$sqlAddLoginToRole = "USE master;ALTER SERVER ROLE [dbcreator] ADD MEMBER [$new_user_username];"
+Invoke-Sqlcmd -Query $sqlAddLoginToRole -TrustServerCertificate
+
+# Get-SQLInstance -ServerInstance "MSSQLSERVER"
+# $server_instance = Get-SQLInstance -ServerInstance "$server"
+# Get-SqlDatabase -ServerInstance "$server"
+
